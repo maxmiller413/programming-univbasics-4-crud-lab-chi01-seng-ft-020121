189e847c1dcfe107777385606870d06449d76b17
Max Miller <maxmiller413@gmail.com>
2021-01-14 11:55:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  create_an_empty_array = []
+  []
 end
 
 def create_an_array
-  create_an_array = ["John", "Paul", "George", "Ringo"]
+  ["John", "Paul", "George", "Ringo"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array << "element"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
